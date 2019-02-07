03d19e40898b402d7cea3c66e47a3b41490af0dc
Carl Waldbieser <cwaldbieser@gmail.com>
2019-02-06 22:06:04 -0500

    Added infix arithmatic.

--- logopy.py
+++ logopy.py
@@ -5,6 +5,18 @@ import string
 import sys
 import parsley
 
+def calculate(start, pairs):
+    result = start
+    for op, value in pairs:
+        if op == '+':
+            result += value
+        elif op == '-':
+            result -= value
+        elif op == '*':
+            result *= value
+        elif op == '/':
+            result /= value
+    return result
 
 def main(args):
     """
@@ -14,22 +26,34 @@ def main(args):
     digit = anything:x ?(x in '0123456789')
     number = <digit+>:ds -> int(ds)
     itemlist = 
-          item:first (ws item)*:rest -> [first] + rest
-        | item:only -> [only]
+          ws item:first (ws item)*:rest -> [first] + rest
+        | ws item:only -> [only]
     item =
           itemlist
         | '[' ws itemlist:lst ws ']' -> lst
         | word:w (ws comment)* -> w
-    word = number | <(word_char+)>:val -> val
+    word = expr | <(word_char+)>:val -> val
     ascii_lower = :x ?(x in 'abcdefghijklmnopqrstuvwxyz') -> x
     ascii_upper = :x ?(x in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') -> x
     ascii = ascii_lower | ascii_upper
     identifier_char = ascii | digit | '.'
-    punctuation = :x ?(x in "!'#$%&\*+,-./:<=>?@^_`" '"')
+    punctuation = :x ?(x in "!'#$%&\,.:<=>?@^_`" '"')
     word_char = ascii | digit | punctuation
     comment = ';' rest_of_line 
     rest_of_line = <('\\\\n' | (~'\\n' anything))*>
-    """, {})
+    parens = '(' ws expr:e ws ')' -> e
+    value = number | parens
+    add = '+' ws expr2:n -> ('+', n)
+    sub = '-' ws expr2:n -> ('-', n)
+    mul = '*' ws value:n -> ('*', n)
+    div = '/' ws value:n -> ('/', n)
+
+    addsub = ws (add | sub)
+    muldiv = ws (mul | div)
+
+    expr = expr2:left addsub*:right -> calculate(left, right)
+    expr2 = value:left muldiv*:right -> calculate(left, right)
+    """, {"calculate": calculate})
     print("Tests ...")
     print(grammar('12345').number())
     print(grammar('print hello world').itemlist())
@@ -39,6 +63,7 @@ def main(args):
     print(grammar('to circle :radius\narc 360 :radius\nend').itemlist())
     print(grammar('print "hello').itemlist())
     print(grammar('print "hello;this is a comment\nshow [a list]').itemlist())
+    print(grammar('print (2 + 3) * 5').itemlist())
     #script = args.file.read()
     #print(grammar(script).itemlist())
 
