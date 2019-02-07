328065b142e142b7bbc0d356af566ac57a912865
Carl Waldbieser <cwaldbieser@gmail.com>
2019-02-07 09:32:00 -0500

    Run tests as an option.

--- logopy.py
+++ logopy.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+import pprint
 import string
 import sys
 import parsley
@@ -26,8 +27,8 @@ def main(args):
     digit = anything:x ?(x in '0123456789')
     number = <'-'{0, 1} digit+>:ds -> int(ds)
     itemlist = 
-          ws item:first (ws item)*:rest -> [first] + rest
-        | ws item:only -> [only]
+          ws item:first (ws item)*:rest ws -> [first] + rest
+        | ws item:only ws -> [only]
     item =
           itemlist
         | '[' ws itemlist:lst ws ']' -> lst
@@ -56,21 +57,23 @@ def main(args):
     expr = expr2:left addsub*:right -> calculate(left, right)
     expr2 = value:left muldiv*:right -> calculate(left, right)
     """, {"calculate": calculate})
-    print("Tests ...")
-    print(grammar('12345').number())
-    print(grammar('print hello world').itemlist())
-    print(grammar('print [hello world]').itemlist())
-    print(grammar('print [hello [nested] world]').itemlist())
-    print(grammar('line1\nline2').itemlist())
-    print(grammar('to circle :radius\narc 360 :radius\nend').itemlist())
-    print(grammar('print "hello').itemlist())
-    print(grammar('print "hello;this is a comment\nshow [a list]').itemlist())
-    print(grammar('print (2 + 3) * 5').itemlist())
-    print(grammar('to equalateral :side [:colors []]').itemlist())
-    print(grammar('localmake "colors (list :color :color :color)').itemlist())
-    print(grammar('make "theta heading * -1 + 90').itemlist())
-    #script = args.file.read()
-    #print(grammar(script).itemlist())
+    if args.tests:
+        print("Tests ...")
+        print(grammar('12345').number())
+        print(grammar('print hello world').itemlist())
+        print(grammar('print [hello world]').itemlist())
+        print(grammar('print [hello [nested] world]').itemlist())
+        print(grammar('line1\nline2').itemlist())
+        print(grammar('to circle :radius\narc 360 :radius\nend').itemlist())
+        print(grammar('print "hello').itemlist())
+        print(grammar('print "hello;this is a comment\nshow [a list]').itemlist())
+        print(grammar('print (2 + 3) * 5').itemlist())
+        print(grammar('to equalateral :side [:colors []]').itemlist())
+        print(grammar('localmake "colors (list :color :color :color)').itemlist())
+        print(grammar('make "theta heading * -1 + 90').itemlist())
+        print("Tests completed.")
+    script = args.file.read()
+    pprint.pprint(grammar(script).itemlist())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Logo programming language interpreter")
@@ -78,5 +81,10 @@ if __name__ == "__main__":
         "file",
         type=argparse.FileType("r"),
         help="Logo script file to interpret.")
+    parser.add_argument(
+        "-t",
+        "--tests",
+        action="store_true",
+        help="Run preliminary tests.")
     args = parser.parse_args()
     main(args)
