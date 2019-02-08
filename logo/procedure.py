dd2b136c78d5026f1a154fb0d7722ac325b006ef
Carl Waldbieser <cwaldbieser@gmail.com>
2019-02-08 01:01:57 -0500

    Added LIST.

--- logo/procedure.py
+++ logo/procedure.py
@@ -64,11 +64,13 @@ def create_primitives_map():
     """
     m = {}
     make_primitive = LogoProcedure.make_primitive
+    m['list'] = make_primitive("list", ['thing1', 'thing2'], [], 'others', 2, process_list)
     m['localmake'] = make_primitive("localmake", ['varname', 'value'], [], None, 2, process_localmake)
     m['make'] = make_primitive("make", ['varname', 'value'], [], None, 2, process_make)
     m['print'] = make_primitive("print", ['thing'], [], 'others', 1, process_print)
     m['pr'] = m['print']
     m['sentence'] = make_primitive("sentence", ['thing1', 'thing2'], [], 'others', 2, process_sentence)
+    m['se'] = m["sentence"]
     m['show'] = make_primitive("show", ['thing'], [], 'others', 1, process_show)
     m['word'] = make_primitive("word", ['word1', 'word2'], [], 'words', 2, process_word)
     return m
@@ -90,6 +92,12 @@ def process_word(logo, *args):
     The WORD command.
     """
     return ''.join(args)
+
+def process_list(logo, *args):
+    """
+    The LIST command.
+    """
+    return list(args)
 
 def process_sentence(logo, *args):
     """
